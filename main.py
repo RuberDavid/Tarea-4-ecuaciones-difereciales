98fa549a17ff47bab99f45c230c33b398f2c212b
luuisraam <57515760+luuisraam@users.noreply.github.com>
2020-10-26 14:11:54 -0600

    Update is_separable.py

--- main.py
+++ main.py
@@ -3,9 +3,29 @@
 
 from sympy import *
 
+# < >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >
 def is_separable( eq ):
-    
-    return True
+    '''
+	[Function]
+	#	Return True if the equation is separable, False if it is not.
+	# 	If f(x,y) = p(x)q(y) then f(1,1) = p(1)q(1)
+	# 	so 			f(1,y)f(x,1) = p(1)q(y)p(x)q(1)
+	# 	therefore 	f(1,y)f(x,1) = f(1,1)f(x,y)
+	#	name: is_separable
+	#  	@param expression
+	#  	@return bool
+	'''
+	# f(1,y)f(x,1)
+	eq_2 = eq.subs(x,1) * eq.subs(y,1)
+
+	# if f(1,y)f(x,1) = f(1,1)f(x,y) is separable
+	if simplify( eq_2 - (eq.subs([(x,1),(y,1)])*eq) ) == 0:
+		return True
+	else:
+		return False
+
+# < >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >
+
 
 def show_resources():
     #TODO
