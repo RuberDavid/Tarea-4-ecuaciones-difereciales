fe8b58a4898845c30895e1dd5ac9766a25324873
luuisraam <57515760+luuisraam@users.noreply.github.com>
2020-10-26 21:42:27 -0600

    Update exact_prueba.py

--- Pruebas/exact_prueba.py
+++ Pruebas/exact_prueba.py
@@ -1,21 +1,13 @@
-
 from sympy import *
 
 x,y = symbols('x y')
 
-# (x**3+2*x*y)/(y*x)
-# -(y/x)
-# -(2*x*E**y+E*x)/((x**2+1)*E**y)
-
-eq = -(2*x+y+10)
+eq = (2*x/y**3) / ( (y**2 - 3*x**2 )/y**4)
 
 eq_2 = eq.subs(x,1)
 eq_3 = eq.subs(y,1)
-print(eq)
-print(eq_2)
-print(eq_3)
 
-if eq_2.has(y) and eq_3.has(x) and eq.has(Pow) :
+if eq_2.has(y) and eq_3.has(x) and (eq.func == Mul or eq.has(Pow)):
     print ("Exacta")
 else:
     print("No es exacta")
